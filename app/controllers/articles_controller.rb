380489c594903c64196096acdef5dbe2edcb8672
Nicole Candiotti <nccandiotti@gmail.com>
2022-03-22 10:12:04 -0400

    commit tree f6af4091690044efbc1cca10001689cb4652aa58

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,13 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    session[:page_views] ||=0
+    session[:page_views] +=1
+    if session[:page_views] <=3
+    
+    render json: Article.find(params[:id])
+    else render json: {error: "Maximum pageview limit reached"}, status: :unauthorized
+    end
   end
 
   private
